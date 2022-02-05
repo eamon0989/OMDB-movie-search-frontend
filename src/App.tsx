69ef62240cf264f57df551dc5a05f7539172c4f2
Eamon <eamon0989@gmail.com>
2022-02-05 16:12:05 +0100

    Addded basic styling using styled components to the nav and search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import './App.css';
-import Body from './Body';
+import { useState } from "react";
+import { MovieListItem } from "../Types";
+import MovieListDetails from "./MovieDetails";
+import SearchForm from "./SearchForm";
+
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: gray;  
+  height: auto;
+  text-align: center;
+`;
+
+const P = styled.p`
+  padding: 2rem;
+  margin: 0;
+`
 
 function App() {
+  const [movies, setMovies] = useState<MovieListItem[]>([]);
+
   return (
     <>
-      <nav>
-        <p>ODMB Search App</p>
-      </nav>
-      <Body />
+      <Nav>
+        <P>ODMB Search App</P>
+      </Nav>
+      <main>
+        <SearchForm setMovies={setMovies}/>
+        {movies.length > 0 && movies.map(movie => <MovieListDetails poster={movie.Poster} title={movie.Title} type={movie.Type} year={movie.Year} imdbID={movie.imdbID} />)}
+      </main>    
     </>
   );
 }
--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import styled from "styled-components";
 import { MovieListItem } from "../Types";
 import getMovieDetailsByTitle from "./services/movies";
 
 const isMovieListItem = function(array: MovieListItem[]): array is MovieListItem[] {
   return (array as MovieListItem[])[0]?.Title !== undefined;
 }
+
+const Form = styled.form`
+  margin: auto;
+  display: flex;
+  padding: 4rem;
+  flex-direction: row;
+  width: fit-content;
+`
 
 const SearchForm = function({ setMovies }: { setMovies: (arg0: MovieListItem[]) => void }) {
   const [title, setTitle] = useState('');
@@ -23,11 +32,11 @@ const SearchForm = function({ setMovies }: { setMovies: (arg0: MovieListItem[]) 
   
   return (
     <>
-      <form onSubmit={(event) => getInfoFromAPI(event)} action="">
+      <Form onSubmit={(event) => getInfoFromAPI(event)} action="">
         <label htmlFor="title">Title</label>
         <input value={title} onChange={(event) => setTitle(event.target.value)} name='title' type="text" />
         <button type='submit'>Submit</button>
-      </form>
+      </Form>
     </>
   )
 }
